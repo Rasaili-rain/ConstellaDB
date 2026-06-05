9b9e19e0b4f7ca33736701873aeb3a1db28776d2
fy-st0rm <slokpradhan2062@gmail.com>
2026-06-06 00:56:05 +0545

    Implemented create table feature

--- src/modules/db_module/src/bin/test.rs
+++ src/modules/db_module/src/bin/test.rs
@@ -1,5 +1,16 @@
-use db_module::add;
+use db_module::*;
 
 fn main() {
-  println!("{}", add(34, 35));
+  let mut engine = Engine::new();
+
+  let user = Table {
+    name: "user_table".to_string(),
+    attrs: vec![
+      Attr { name: "id".to_string(), data_type: Type::Int },
+      Attr { name: "name".to_string(), data_type: Type::VarChar(100) },
+      Attr { name: "password".to_string(), data_type: Type::VarChar(8) },
+    ]
+  };
+
+  engine.create_table(&user).unwrap();
 }
--- src/modules/db_module/src/lib.rs
+++ src/modules/db_module/src/lib.rs
@@ -1,14 +1,116 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fs::{self, File};
+use std::path::{Path, PathBuf};
+use std::result::Result;
+use std::io::{BufWriter, BufReader};
+use serde::{Deserialize, Serialize};
+
+
+pub const DB_DIR: &str = "DB";
+pub const SCHEMA_FILE: &str = "schemas.json";
+
+#[derive(Clone, Serialize, Deserialize)]
+pub enum Type {
+  Int,
+  VarChar(usize),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Attr {
+  pub name: String,
+  pub data_type: Type,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Table {
+  pub name: String,
+  pub attrs: Vec<Attr>,
+}
+
+pub struct Engine {
+  tables: Vec<Table>,
+}
+
+impl Engine {
+  pub fn new() -> Self {
+    if !Path::new(DB_DIR).exists() {
+
+      fs::create_dir(DB_DIR).unwrap();
+      File::create(
+        PathBuf::from(DB_DIR)
+          .join(SCHEMA_FILE)
+          .to_str()
+          .unwrap()
+      ).unwrap();
+
+      return Self {
+        tables: Vec::new(),
+      };
     }
+
+    return Self {
+        tables: Self::load_schema(),
+    }
+  }
+
+  pub fn create_table(&mut self, table: &Table) -> Result<(), String> {
+    if self.table_exists(&table.name) {
+      return Err(format!("Table with name {} already exists", table.name));
+    }
+
+    // Create the table dir
+    let table_dir_path = PathBuf::from(DB_DIR)
+      .join(&table.name);
+
+    fs::create_dir_all(table_dir_path.to_str().unwrap()).unwrap();
+
+    // Create attribute files
+    for attr in table.attrs.iter() {
+      let attr_file_path = table_dir_path
+        .clone()
+        .join(format!("{}.col", attr.name));
+
+      File::create(attr_file_path.to_str().unwrap()).unwrap();
+    }
+
+    // Store the table
+    self.tables.push(table.clone());
+
+    // Save the schema
+    self.save_schema();
+
+    Ok(())
+  }
+
+  fn save_schema(&self) {
+    let file = File::create(
+      PathBuf::from(DB_DIR)
+        .join(SCHEMA_FILE)
+        .to_str()
+        .unwrap()
+    ).unwrap();
+
+    let writer = BufWriter::new(file);
+    serde_json::to_writer_pretty(writer, &self.tables).unwrap();
+  }
+
+  fn load_schema() -> Vec<Table> {
+    let file = File::open(
+      PathBuf::from(DB_DIR)
+        .join(SCHEMA_FILE)
+        .to_str()
+        .unwrap()
+    ).unwrap();
+
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader).unwrap()
+  }
+
+  fn table_exists(&self, name: &str) -> bool {
+    for table in self.tables.iter() {
+      if table.name == name {
+        return true;
+      }
+    }
+    false
+  }
 }
